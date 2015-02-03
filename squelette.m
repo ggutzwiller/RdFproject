d6a1cd1ed973de424aea4c0bdeb14e404800d3bc
Baron Julien <jbaron@asi-testing>
2015-02-03 12:42:56 +0100

    choix kppv ajouté

--- squelette.m
+++ squelette.m
@@ -8,7 +8,7 @@ close all; clc; clear all;
 im = imread('app.tif'); % lecture fichier image d'apprentissage
 coordImages = extractionImages(im); 
 nbImageBaseApp = length(coordImages);
-sprintf('APPRENTISSAGE detection images OK : %d images detect�es\n', nbImageBaseApp);
+sprintf('APPRENTISSAGE detection images OK : %d images detectees\n', nbImageBaseApp);
 
 m = 5;
 n=5;%nb de traits
@@ -27,9 +27,8 @@ for (iImage=1 : nbImageBaseApp)
   
     % crop (supprimer les bords blancs)
     imageChiffreCroppee = crop(imageChiffre);    
-    imagesc(imageChiffreCroppee); %afficher les imagettes de chiffres    
+    %imagesc(imageChiffreCroppee); %afficher les imagettes de chiffres    
     
-    %%%%%% ICI c'est à vous de Jouer !!!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % extraire des caractéristiques ...
     lesprofils(:,iImage) = extraitProfils(imageChiffreCroppee, n);
     lesdensites(:,:,iImage) = extraitDensites(imageChiffreCroppee, m, n);
@@ -37,10 +36,9 @@ for (iImage=1 : nbImageBaseApp)
     modele(:,iImage) = [lesprofils(:,iImage)' reshape(lesdensites(:,:,iImage), 1, m*n)]    
     % le sauvegarder ...
     
-    % Astuce : la classe de l'image courante est donn�e par : iClasse = fix((iImage-1)/20)
+    % Astuce : la classe de l'image courante est donnee par : iClasse = fix((iImage-1)/20)
     sprintf('classe de l image %d : %d\n', iImage, fix((iImage-1)/20))
     
-    %%%%%%%%%%%%%%%%%%%%%%
 end
 
 
@@ -68,8 +66,8 @@ for (iImage=1 : nbImageBaseTest)
     
     % crop
     imageChiffreCroppee = crop(imageChiffre);    
-    imagesc(imageChiffreCroppee); %afficher les imagettes de chiffres
-    %%%%%% ICI c'est à vous de Jouer !!!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %imagesc(imageChiffreCroppee); %afficher les imagettes de chiffres
+
     % appliquer le modèle sauvegardé sur les chiffres de l'image de test ...
     caracImage = [extraitProfils(imageChiffreCroppee, n) reshape(extraitDensites(imageChiffreCroppee, m, n), 1, m*n)];
     distance = norm(modeleDEM(1,:)-caracImage, 2);
@@ -80,7 +78,7 @@ for (iImage=1 : nbImageBaseTest)
             k(iImage) = i-1;
         end
     end
-    %%%%%%%%%%%%%%%%%%%%%%
+
 end
 
 %% Calcul des performances euclid %%%%%%%%
@@ -96,42 +94,42 @@ confusionDEM = confusionDEM / 10;
 
 confusionDEM 
 
+
+
 %% decision kppv %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 imTest = imread('test.tif'); % lecture fichier image test
 coordImagesTest = extractionImages(imTest);
 length(coordImagesTest)
 nbImageBaseTest = length(coordImagesTest);
-kp = 5;
+for tests=1:5
+    kp=tests;
+    for (iImage=1 : nbImageBaseTest)
+        largeur = coordImagesTest(iImage, 2) - coordImagesTest(iImage, 1) - 2;
+        hauteur = coordImagesTest(iImage, 4) - coordImagesTest(iImage, 3) - 2;
 
-for (iImage=1 : nbImageBaseTest)
-    largeur = coordImagesTest(iImage, 2) - coordImagesTest(iImage, 1) - 2;
-    hauteur = coordImagesTest(iImage, 4) - coordImagesTest(iImage, 3) - 2;
-    
-    % extraction image
-    imageChiffre = subimage(imTest, largeur, hauteur, coordImagesTest(iImage, 1), coordImagesTest(iImage, 3));
-    
-    % crop
-    imageChiffreCroppee = crop(imageChiffre);    
-    imagesc(imageChiffreCroppee); %afficher les imagettes de chiffres
-    %%%%%% ICI c'est à vous de Jouer !!!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % appliquer le modèle sauvegardé sur les chiffres de l'image de test ...
-    caracImage = [extraitProfils(imageChiffreCroppee, n) reshape(extraitDensites(imageChiffreCroppee, m, n), 1, m*n)];
-    distance = zeros(2,200)
-    
-    distance(:, 1) = [0 norm(modele(:,1)'-caracImage, 2)];
-    for i=2:200
-        distance(:,i) = [floor((i-1)/20) norm(modele(:,i)'-caracImage, 2)];
+        % extraction image
+        imageChiffre = subimage(imTest, largeur, hauteur, coordImagesTest(iImage, 1), coordImagesTest(iImage, 3));
+
+        % crop
+        imageChiffreCroppee = crop(imageChiffre);    
+        %imagesc(imageChiffreCroppee); %afficher les imagettes de chiffres
+
+        % appliquer le modèle sauvegardé sur les chiffres de l'image de test ...
+        caracImage = [extraitProfils(imageChiffreCroppee, n) reshape(extraitDensites(imageChiffreCroppee, m, n), 1, m*n)];
+        distance = zeros(2,200)
+
+        distance(:, 1) = [0 norm(modele(:,1)'-caracImage, 2)];
+        for i=2:200
+            distance(:,i) = [floor((i-1)/20) norm(modele(:,i)'-caracImage, 2)];
+        end
+
+        distb = sortrows(distance', 2)';
+
+        resultats(iImage) = mode(distb(1,1:kp));
+
     end
-    
-    distb = sortrows(distance', 2)';
-    
-    resultats(iImage) = mode(distb(1,1:kp));
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%
-end
 
-%% Calcul des performances kppv %%%%%%%%
+%%% Calcul des performances kppv %%%%%%%%
 
 confusionKPPV = zeros(10,10)
 
@@ -142,9 +140,15 @@ for i=1:10
 end
 confusionKPPV = confusionKPPV / 10;
 
-confusionDEM
-confusionKPPV
+succes(tests,:) =  diag(confusionKPPV);
 
+end
+
+succes
+
+succesmoy = mean(succes')'
+
+% Commentaire on qu'on optimal pour k = 2
 %% *Annexe 1* : Code MATLAB
 % Voici une copie du code matlab qui vient d'être exécuté :
 
