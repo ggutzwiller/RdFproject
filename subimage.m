3f5879cd587932428e3feaa412574921b7a1f0f7
Julien BARON <code@jbaron.fr>
2015-02-23 22:30:02 +0100

    fix(subimage) : delete those fucking "for"

--- subimage.m
+++ subimage.m
@@ -1,10 +1,11 @@
 function s = subimage(imageMere, largeur, hauteur, xDeb, yDeb)
-s = zeros(hauteur, largeur);
 
-[dy, dx] = size(s);
+s = imageMere(yDeb : yDeb + hauteur , xDeb : xDeb + largeur );
 
-for r = 1:largeur 
-    for c = 1:hauteur 
-        s(c, r) = imageMere(yDeb + c, xDeb + r);
-    end
-end 
+% [dy, dx] = size(s);
+% 
+% for r = 1:largeur 
+%     for c = 1:hauteur 
+%         s(c, r) = imageMere(yDeb + c, xDeb + r);
+%     end
+% end 
